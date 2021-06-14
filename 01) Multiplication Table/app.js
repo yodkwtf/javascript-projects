761e6c8255562ca0e99d4ed09cbd5e9ef4df4395
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-06-14 19:58:40 +0530

    muliplication table updated

--- 01) Multiplication Table/app.js
+++ 01) Multiplication Table/app.js
@@ -1,53 +1,17 @@
-const section = document.querySelector('.section')
+const section = document.querySelector('.section');
 const table = document.querySelector('.table');
 const input = document.getElementById('input');
 const printBtn = document.getElementById('print');
-const prevBtn = document.getElementById('prev');
-const nextBtn = document.getElementById('next');
 
-
-document.addEventListener('keydown', function(e) {
-
-  // A) Enter is pressed - Print
+document.addEventListener('keydown', function (e) {
+  // When enter is pressed - Print
   if (e.keyCode === 13 || e.which === 13) {
     displayTable();
   }
-  // B) When left is pressed - Prev
-  if (e.keyCode === 37 || e.which === 37 || e.keyCode === 40 || e.which === 40) {
-    if (input.value > 1) {
-      input.value--;
-    }
-    displayTable();
-  }
-  // C) when right is pressed - Next
-  if (e.keyCode === 39 || e.which === 39 || e.keyCode === 38 || e.which === 38) {
-    input.value++;
-    displayTable();
-  }
-})
-
+});
 
 // Clicking on print btn
 printBtn.addEventListener('click', displayTable);
-
-
-// Clicking on prev btn
-prevBtn.addEventListener('click', function () {
-  if(input.value > 1) {
-    input.value--;
-  }
-  displayTable();
-});
-
-
-// Clicking on next btn
-nextBtn.addEventListener('click', function() {
-  input.value++;
-  displayTable();
-});
-
-
-
 
 // Display table based on currnt input value
 function displayTable() {
@@ -58,10 +22,8 @@ function displayTable() {
   }
 }
 
-
 // Print table based on input value
 function printTable(n) {
-
   // 1) Display hidden items
   display();
   // 2) Create an output to put the table content inside
@@ -78,13 +40,10 @@ function printTable(n) {
   }
   // 6) Append to table div
   table.innerHTML = output;
-
 }
-
 
 // Show error on a invalid input
 function showError() {
-
   // 1) Create an element
   const h2 = document.createElement('h2');
   // 2) Assign an error class for style
@@ -97,12 +56,9 @@ function showError() {
   setTimeout(function () {
     h2.remove();
   }, 3000);
-
 }
-
 
 // Displaying hidden items
 function display() {
   table.style.display = 'block';
-  document.querySelector('.btn-container').style.display = 'block';
 }
---
+++ 01) Multiplication Table/readme.md
@@ -0,0 +1,15 @@
+## Multiplication Table
+
+A multiplication table app created with vanillaJS and a bit of CSS. The sole purpose of this app was to practice DOM manipulation and functions in javascript.
+
+## Live Preview
+
+[Multiplication Table](https://multiplicationtable-dk.netlify.app/)
+
+## Things to Learn
+
+- HTML & CSS
+- Javascript
+- Events in javascript
+- Functions in javascript
+- DOM Manipulation
