4bc90bbb5ff71335dea3dc08156ebde40e3bed98
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-11-08 23:20:47 +0530

    functionality improved with click to animate

--- 07) Text Animation/app.js
+++ 07) Text Animation/app.js
@@ -1,29 +1,37 @@
-// get h1
-const textElement = document.querySelector('.sp-text');
-// get all letters inside
-const letters = textElement.textContent.split('');
-// erase the text
-textElement.textContent = '';
+function init() {
+  // get h1
+  const textElement = document.querySelector('.sp-text');
+  // get all letters inside
+  const letters = textElement.textContent.split('');
+  // erase the text
+  textElement.textContent = '';
 
-// wrap each letter inside a span
-letters.forEach((letter) => {
-  textElement.innerHTML += `<span>${letter}</span>`;
-});
+  // wrap each letter inside a span
+  letters.forEach((letter) => {
+    textElement.innerHTML += `<span>${letter}</span>`;
+  });
 
-let i = 0;
+  let i = 0;
 
-//-ADD ANIMATION FUNCTION
-const addAnimation = () => {
-  const span = document.querySelectorAll('span')[i];
-  span.classList.add('fade');
-  i++;
-  if (i >= letters.length) {
-    return complete();
-  }
-};
+  //-ADD ANIMATION FUNCTION
+  const addAnimation = () => {
+    const span = document.querySelectorAll('span')[i];
+    span.classList.add('animated');
+    i++;
+    if (i >= letters.length) {
+      return complete();
+    }
+  };
 
-// ADD ANIMATION TO LETTERS ONE BY ONE
-const interval = setInterval(addAnimation, 100);
+  // ADD ANIMATION TO LETTERS ONE BY ONE
+  const interval = setInterval(addAnimation, 100);
 
-//-COMPLETE ANIMATION FUNCTION
-const complete = () => clearInterval(interval);
+  //-COMPLETE ANIMATION FUNCTION
+  const complete = () => clearInterval(interval);
+}
+
+init();
+
+// animate on click
+const animateBtn = document.querySelector('.btn');
+animateBtn.addEventListener('click', init);
